49fe6e14e327ed902b29afad16d75095ef1484f3
palmerimatthew <37392128+palmerimatthew@users.noreply.github.com>
2023-06-07 08:56:52 -0500

    Added helper function to get player id and player name

--- R/individual_player_scraper.R
+++ R/individual_player_scraper.R
@@ -5,8 +5,12 @@ player_info_scraper <- function(website) {
   messy_info_table <- page %>%
     rvest::html_elements("div[class='ep-list']") %>%
     rvest::html_elements("div[class^='order']") %>%
-    purrr::map_df(., .f = .player_info_cleaner) %>%
-    dplyr::filter(Attribute != "Highlights")
+    purrr::map_df(., .f = .player_info_cleaner)
+
+  clean_info_table <- messy_info_table %>%
+    dplyr::filter(attribute != "Highlights") %>%
+    dplyr::mutate(attribute = tolower(gsub(" ", "_", attribute))) %>%
+    tidyr::pivot_wider(names_from = "attribute", values_from = "value")
 }
 
 .player_info_cleaner <- function(html_node) {
@@ -14,9 +18,28 @@ player_info_scraper <- function(website) {
     rvest::html_children() %>%
     rvest::html_text() %>%
     trimws() %>%
-    set_names(c("Attribute", "Value")) %>%
+    set_names(c("attribute", "value")) %>%
     t() %>%
     data.frame()
+}
+
+
+.player_name <- function(website, page) {
+  player_id <- stringr::str_split(website, "/")[[1]] %>%
+    Filter(.numeric_identifier, .) %>%
+    as.numeric()
+  player_name <- page %>%
+    rvest::html_elements("h1[class$='name']") %>%
+    rvest::html_text() %>%
+    trimws()
+
+  return(data.frame(id = player_id, name = player_name))
+}
+
+.numeric_identifier <- function(x) {
+  suppressWarnings(as.numeric(x)) %>%
+    is.na() %>%
+    isFALSE()
 }
 
 
