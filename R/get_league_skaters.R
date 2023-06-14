7de6f09069da3354a9bdf465dc08bb37659b514e
palmerimatthew <37392128+palmerimatthew@users.noreply.github.com>
2023-06-13 19:54:27 -0500

    added initial code for main function

--- R/get_league_skaters.R
+++ R/get_league_skaters.R
@@ -9,4 +9,34 @@ get_league_skaters <- function(website = NULL, league = "NHL", season = "2022-20
   if (is.null(website) & (is.null(league) | is.null(season))) {
     stop("Please supply both `league` and `season` parameters")
   }
+
+  if (!is.null(league) & !is.null(season)) {
+    website <- paste0("https://www.eliteprospects.com/league/",
+                      tolower(league), "/stats/", season)
+  }
+
+  #getting number of pages
+  page <- rvest::read_html(website)
+  number_of_pages <- page %>%
+    rvest::html_elements("div[id='skater-stats']") %>%
+    rvest::html_elements("div[class='table-pagination']") %>%
+    rvest::html_text() %>%
+    stringr::str_extract("[0-9]+ players found") %>%
+    stringr::str_extract("[0-9]+") %>%
+    as.numeric() %>%
+    magrittr::divide_by(100) %>%
+    ceiling()
+
+  full_player_list <- purrr::map_df(1:number_of_pages, .f = function(page_num) {
+    .get_league_skaters_helper(paste0(website, "?page=", page_num))
+  })
+
+
+
+}
+
+
+
+.get_league_skaters_helper <- function(website) {
+
 }
