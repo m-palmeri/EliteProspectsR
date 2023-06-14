5b45e7aa524eb9383f6b454d4c485f8a4b2aceb6
m-palmeri <matthew.thomas.palmeri@gmail.com>
2023-06-14 16:11:34 -0500

    various adjustments to get function to also work with all-time pages

--- R/get_league_skaters.R
+++ R/get_league_skaters.R
@@ -1,7 +1,16 @@
 get_league_skaters <- function(website = NULL, league = "NHL", season = "2022-2023") {
+
+  function_call <- match.call()
+  # setting league and season to NULL if only website is specified
+  if ("website" %in% names(function_call) &
+      all(!(c("league", "season") %in% names(function_call)))) {
+    league <- NULL
+    season <- NULL
+  }
+
   #input checks
   if (is.null(website) & is.null(league) & is.null(season)) {
-    stop("Please use either the `website` parameter, or the `league` and `season` parameters, not both")
+    stop("Please specify either the full website, or the league and season")
   }
   if (!is.null(website) & (!is.null(league) | !is.null(season))) {
     stop("Please use either the `website` parameter, or the `league` and `season` parameters, not both")
@@ -21,7 +30,8 @@ get_league_skaters <- function(website = NULL, league = "NHL", season = "2022-20
     rvest::html_elements("div[id='skater-stats']") %>%
     rvest::html_elements("div[class='table-pagination']") %>%
     rvest::html_text() %>%
-    stringr::str_extract("[0-9]+ players found") %>%
+    gsub(" ", "", .) %>%
+    stringr::str_extract("[0-9]+playersfound") %>%
     stringr::str_extract("[0-9]+") %>%
     as.numeric() %>%
     magrittr::divide_by(100) %>%
@@ -52,21 +62,25 @@ get_league_skaters <- function(website = NULL, league = "NHL", season = "2022-20
                   goals = G,
                   assists = A,
                   points = TP,
-                  penalty_minutes = PIM,
-                  plus_minus = `+/-`) %>%
+                  penalty_minutes = PIM) %>%
     dplyr::rename_with(tolower)
+  names(skater_table) <- gsub("\\+/-", "plus_minus", names(skater_table))
 
   player_links <- .get_league_skater_links(table_setup, "player")
+  skater_table <- dplyr::mutate(skater_table, player_link = player_links)
+  char_vec <- c("player", "player_link")
 
-  team_links <- .get_league_skater_links(table_setup, "team")
+  if ("team" %in% names(skater_table)) {
+    team_links <- .get_league_skater_links(table_setup, "team")
+    skater_table <- dplyr::mutate(skater_table, team_link = team_links)
+    char_vec <- c(char_vec, "team", "team_link")
+  }
 
   full_skater_table <- skater_table %>%
-    dplyr::mutate(player_link = player_links,
-                  team_link = team_links) %>%
-    dplyr::filter(team != "") %>%
-    dplyr::select(rank, player, player_link, team, team_link, tidyselect::everything()) %>%
-    dplyr::mutate(dplyr::across(player:team_link, as.character),
-                  dplyr::across(games_played:plus_minus, as.numeric))
+    dplyr::filter(games_played != "") %>%
+    dplyr::select(rank, tidyselect::all_of(char_vec), tidyselect::everything()) %>%
+    dplyr::mutate(dplyr::across(tidyselect::all_of(char_vec), as.character),
+                  dplyr::across(games_played:tidyselect::last_col(), as.numeric))
 
   return(full_skater_table)
 }
