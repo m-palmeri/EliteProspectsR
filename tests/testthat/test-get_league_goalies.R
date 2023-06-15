75e75c2ab406c2b1f78d768592ea2484f432445a
m-palmeri <matthew.thomas.palmeri@gmail.com>
2023-06-14 20:42:55 -0500

    added test files for get_league_skaters and get_league_goalies

---
+++ tests/testthat/test-get_league_goalies.R
@@ -0,0 +1,59 @@
+
+test_that("error catching", {
+  expect_error(get_league_goalies(website = NULL, league = NULL, season = NULL))
+  expect_error(get_league_goalies(website = "test", league = "test", season = NULL))
+  expect_error(get_league_goalies(league = "NHL", season = NULL))
+})
+
+
+test_that("parameter check", {
+  #using 2010 nhl as check
+  with_website <- get_league_goalies(website = "https://www.eliteprospects.com/league/nhl/stats/2010-2011")
+
+  with_uppercase <- get_league_goalies(league = "NHL", season = "2010-2011")
+
+  with_lowercase <- get_league_goalies(league = "nhl", season = "2010-2011")
+
+  comp <- load_test_rds("nhl_2010_2011_goalies.rds")
+
+  expect_equal(with_website, comp)
+  expect_equal(with_uppercase, comp)
+  expect_equal(with_lowercase, comp)
+
+})
+
+
+test_that("WHL 2015 check", {
+  whl_2015 <- get_league_goalies(league = "WHL", season = "2015-2016")
+
+  comp <- load_test_rds("whl_2015_2016_goalies.rds")
+
+  expect_equal(whl_2015, comp)
+})
+
+
+test_that("Liiga 2008 check", {
+  liiga_2008 <- get_league_goalies(league = "Liiga", season = "2008-2009")
+
+  comp <- load_test_rds("liiga_2008_2009_goalies.rds")
+
+  expect_equal(liiga_2008, comp)
+})
+
+test_that("WJC-20 2014 check", {
+  wjc_2014 <- get_league_goalies(league = "wjc-20", season = "2014-2015")
+
+  comp <- load_test_rds("wjc_2014_2015_goalies.rds")
+
+  expect_equal(wjc_2014, comp)
+})
+
+
+test_that("WHA All-Time check", {
+  wha_alltime <- get_league_goalies(league = "WHA", season = "all-time")
+
+  comp <- load_test_rds("wha_alltime_goalies.rds")
+
+  expect_equal(wha_alltime, comp)
+})
+
