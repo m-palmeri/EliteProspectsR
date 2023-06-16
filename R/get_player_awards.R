d04180fecfd7e8655065baea619fdf1d876d5c81
m-palmeri <matthew.thomas.palmeri@gmail.com>
2023-06-15 20:02:52 -0500

    initial code for get_player_awards

---
+++ R/get_player_awards.R
@@ -0,0 +1,46 @@
+get_player_awards <- function(website) {
+  page <- rvest::read_html(website)
+
+  awards_temp_list <- page %>%
+    rvest::html_elements("div[id='awards']") %>%
+    rvest::html_elements("li")
+
+  #subsetting to overaching lists
+  overarching_list <- awards_temp_list %>%
+    rvest::html_element("div") %>%
+    purrr::map(function(x) !is.na(x)) %>%
+    unlist()
+
+  awards_df <- purrr::map_df(
+    awards_temp_list[which(overarching_list)],
+    .get_player_highlights_helper
+  )
+
+  return(awards_df)
+}
+
+.get_player_awards_helper <- function(li) {
+  season <- li %>%
+    rvest::html_elements("div[class$='season']") %>%
+    rvest::html_text() %>%
+    stringr::str_extract(., "[0-9-]+")
+
+  awards <- li %>%
+    rvest::html_elements("li") %>%
+    rvest::html_text() %>%
+    gsub("\\(.*\\)", "", .) %>%
+    trimws()
+
+  award_links <- li %>%
+    rvest::html_elements("li") %>%
+    rvest::html_element("a") %>%
+    rvest::html_attr("href")
+
+  df <- data.frame(
+    season = season,
+    award = awards,
+    award_link = award_links
+  )
+
+  return(df)
+}
