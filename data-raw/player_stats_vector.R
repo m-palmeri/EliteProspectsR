684f88016ce97f62b0484e75599f6429b1e7e5b8
m-palmeri <matthew.thomas.palmeri@gmail.com>
2023-06-16 09:50:33 -0500

    added internal vector for column renames

---
+++ data-raw/player_stats_vector.R
@@ -0,0 +1,22 @@
+
+player_stats_vector <- list(
+  season = "S",
+  games_played = "GP",
+  goals = "G",
+  assists = "A",
+  points = "TP",
+  penalty_minutes = "PIM",
+  plus_minus = "+/-",
+  points_per_game = "PPG",
+  games_dressed = "GD",
+  goals_against_average = "GAA",
+  save_percentage = "SV%",
+  goals_against = "GA",
+  saves = "SV",
+  shutouts = "SO",
+  record = "WLT",
+  time_on_ice = "TOI"
+)
+player_stats_vector <- unlist(player_stats_vector)
+
+usethis::use_data(player_stats_vector, internal = T, overwrite = T)
