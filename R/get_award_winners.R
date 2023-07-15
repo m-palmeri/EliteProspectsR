16c8ca7888104bb045ac4cd7727d1f3085066327
m-palmeri <matthew.thomas.palmeri@gmail.com>
2023-07-15 09:57:35 -0500

    initial code for get_award_winners

---
+++ R/get_award_winners.R
@@ -0,0 +1,25 @@
+get_award_winners <- function(website) {
+  page <- rvest::read_html(website)
+
+  table_setup <- page %>%
+    rvest::html_elements("div[id='all-players-awards']") %>%
+    rvest::html_elements("table")
+
+  player_links <- .get_table_links(table_setup, "player")
+
+  award_table <- table_setup %>%
+    rvest::html_table() %>%
+    magrittr::extract2(1) %>%
+    `colnames<-`(ifelse(names(.) == "", "test", names(.))) %>%
+    dplyr::rename(season = Season,
+                  player = `Award winner`) %>%
+    dplyr::select(season, player) %>%
+    dplyr::mutate(player = gsub("\\(.*\\)$", "", player),
+                  player = trimws(player),
+                  player_link = player_links,
+                  season = dplyr::if_else(season == "", NA, season)) %>%
+    tidyr::fill(season)
+
+  return(award_table)
+}
+
