001a84a496fff332b5a98c81c1ed6295124c412d
Matthew Palmeri <matthew.thomas.palmeri@gmail.com>
2023-07-21 16:51:54 -0500

    Added staff_info_vector to internal package data

--- data-raw/internal_data.R
+++ data-raw/internal_data.R
@@ -3,16 +3,15 @@ library(magrittr)
 ### player_info_vector             -----
 website <- "https://www.eliteprospects.com/player/183442/connor-mcdavid"
 
-page <- rvest::read_html(website)
-
-player_info_vector <- page %>%
+player_info_vector <- rvest::read_html(website) %>%
   rvest::html_elements("div[class='ep-list']") %>%
   rvest::html_elements("div[class^='order']") %>%
-  purrr::map_df(., .f = .player_info_cleaner) %$%
+  purrr::map_df(., .f = .person_info_cleaner) %$%
   attribute %>%
   c(., "Status")
 
-rm(website, page)
+rm(website)
+
 
 ### player_stats_vector            -----
 player_stats_vector <- list(
@@ -47,6 +46,23 @@ player_stats_vector <- unlist(player_stats_vector)
 ### player_stats_vector_character -----
 player_stats_vector_character <- c("season", "record")
 
+### staff_info_vector             -----
+website <- "https://www.eliteprospects.com/staff/4749/joe-sakic"
+
+staff_info_vector <- rvest::read_html(website) %>%
+  rvest::html_elements("div[class='ep-card']") %>%
+  rvest::html_elements("div[class='ep-list']") %>%
+  rvest::html_children() %>%
+  purrr::map_df(., .person_info_cleaner) %$%
+  attribute
+
+rm(website)
+
 ### write data                    ------
 
-usethis::use_data(player_info_vector, player_stats_vector, player_stats_vector_character, internal = T, overwrite = T)
+usethis::use_data(player_info_vector,
+                  player_stats_vector,
+                  player_stats_vector_character,
+                  staff_info_vector,
+                  internal = T,
+                  overwrite = T)
