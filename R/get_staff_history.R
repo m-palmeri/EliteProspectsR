dcbfb503e9421df3355ef4f38cf4eb02ec2d2d56
m-palmeri <matthew.thomas.palmeri@gmail.com>
2023-07-23 17:57:39 -0500

    added staff_id and team_id columns to output

--- R/get_staff_history.R
+++ R/get_staff_history.R
@@ -1,15 +1,14 @@
 get_staff_history <- function(website) {
   page <- rvest::read_html(website)
 
-  staff_id <- stringr::str_split(website, "/")[[1]] %>%
-    Filter(.numeric_identifier, .) %>%
-    as.numeric()
+  staff_id <- .get_website_id(website)
 
   table_setup <- page %>%
     rvest::html_elements("div[id='staff-stats']") %>%
     rvest::html_elements("table")
 
   team_links <- .get_table_links(table_setup, "team")
+  team_ids <- .get_website_id(team_links)
   league_links <- .get_table_links(table_setup, "league")
 
   staff_history <- table_setup %>%
@@ -20,8 +19,9 @@ get_staff_history <- function(website) {
     dplyr::rename_with(tolower) %>%
     replace(., . == "", NA) %>%
     tidyr::fill(season) %>%
-    dplyr::mutate(team_link = team_links,
+    dplyr::mutate(team_id = team_ids,
+                  team_link = team_links,
                   league_link = league_links,
                   staff_id = staff_id) %>%
-    dplyr::select(staff_id, season, team, team_link, league, league_link, role, notes)
+    dplyr::select(staff_id, season, team_id, team, team_link, league, league_link, role, notes)
 }
