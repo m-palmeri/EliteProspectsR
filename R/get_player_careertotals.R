1bba55470467fbdcc515a2a4804c71766d5b1e24
m-palmeri <matthew.thomas.palmeri@gmail.com>
2023-07-23 17:32:22 -0500

    added player_id and name columns to get_player_careertotals

--- R/get_player_careertotals.R
+++ R/get_player_careertotals.R
@@ -1,6 +1,9 @@
 get_player_careertotals <- function(website) {
   page <- rvest::read_html(website)
 
+  player_id <- .get_website_id(website)
+  name <- .get_person_name(page)
+
   table_setup <- page %>%
     rvest::html_elements("div[id='total-stats']") %>%
     rvest::html_elements("table[class*='total-player-stats']")
@@ -8,23 +11,24 @@ get_player_careertotals <- function(website) {
     rvest::html_table(fill = TRUE) %>%
     magrittr::extract2(1) %>%
     magrittr::set_names(gsub("^$", "NA", names(.))) %>%
-    cbind(.player_name(website, page), .)
+    cbind(name, .) %>%
+    cbind(player_id, .)
 
   league_links <- .get_table_links(table_setup, "league")
 
   player_tables <- list(
-    dplyr::select(raw_player_table, id:`|`) %>%
+    dplyr::select(raw_player_table, player_id:`|`) %>%
       dplyr::select(., -dplyr::last_col()),
-    dplyr::select(raw_player_table, id:League, `|`:dplyr::last_col()) %>%
+    dplyr::select(raw_player_table, player_id:League, `|`:dplyr::last_col()) %>%
       dplyr::select(-`|`)
   )
 
   clean_player_table <- purrr::map(player_tables, .player_stats_cleaner) %>%
-    magrittr::set_names(c("Regular Season", "Playoffs")) %>%
+    magrittr::set_names(c("regular_season", "playoffs")) %>%
     purrr::map(., .f = function(df) {
       df %>%
         dplyr::mutate(league_link = league_links) %>%
-        dplyr::select(id:league, league_link, tidyselect::everything())
+        dplyr::select(player_id:league, league_link, tidyselect::everything())
     }) %>%
     Filter(function(x) nrow(x) > 0, .)
 }
