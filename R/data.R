14470d3dc4fa535b54e472611b064f81afe378a4
m-palmeri <matthew.thomas.palmeri@gmail.com>
2023-07-29 19:54:28 -0500

    added draft_names_crosswalk

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Eliteprospects draft list
+#'
+#' A crosswalk of league drafts to the link suffixes for easy website creation
+#' and lookup
+#'
+#' @format ## `draft_names_crosswalk`
+#' A data frame with 41 rows and 2 columns:
+#' \describe{
+#'   \item{draft_name}{official name of the draft}
+#'   \item{link_suffix}{link embedded variable to demark particular draft}
+#' }
+#' @source <https://www.eliteprospects.com/draft/nhl-entry-draft>
+"draft_names_crosswalk"
---
+++ data-raw/draft_names_crosswalk.R
@@ -0,0 +1,30 @@
+## code to prepare `draft_names_crosswalk` dataset goes here
+
+page <- rvest::read_html("https://www.eliteprospects.com/draft/nhl-entry-draft")
+
+get_draft_links_internal_data <- function(option_node) {
+  draft_name <- option_node %>%
+    rvest::html_text()
+  link_suffix <- option_node %>%
+    rvest::html_attr("value") %>%
+    stringr::str_split("/") %>%
+    magrittr::extract2(1) %>%
+    tail(., 1)
+  return(data.frame(draft_name, link_suffix))
+}
+
+draft_names_crosswalk <- page %>%
+  rvest::html_elements("select") %>%
+  Filter(function(x) {  # filtering to the one with draft websites
+    rvest::html_children(x) %>%
+      magrittr::extract(2) %>%
+      rvest::html_attr("value") %>%
+      grepl("www.+com", .)
+  }, .) %>%
+  rvest::html_elements("option[value]") %>%
+  purrr::map_df(., get_draft_links_internal_data) %>%
+  dplyr::mutate_all(trimws)
+
+rm(page, get_draft_links_internal_data)
+
+usethis::use_data(draft_names_crosswalk, overwrite = TRUE)
